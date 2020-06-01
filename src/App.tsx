777c39498d669b0ad6863bf1e5b90350d6edf3b9
Brian Czaicki <bczaicki@outlook.com>
2020-06-01 01:05:17 -0500

    Adding to gitignore and adding copy functionality

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,15 @@ function App() {
     setTextValue("");
   };
 
+  const copyClicked = () => {
+    const copyText = document.getElementById("typeWritterField");
+    if (!copyText) {
+      alert("Sorry, could not copy text");
+    } else {
+      navigator.clipboard.writeText(copyText.innerHTML);
+    }
+  };
+
   const handleChange = (change: KeyboardEvent<HTMLDivElement>) => {
     if (
       ((change.keyCode >= 48 && change.keyCode <= 90) || // Letters & Numbers
@@ -33,7 +42,7 @@ function App() {
   return (
     <div className="App">
       <button onClick={clearClicked}>Clear</button>
-      <button>Copy</button>
+      <button onClick={copyClicked}>Copy</button>
       <div>WordCount: {wordcount}</div>
       <TextField
         multiline
