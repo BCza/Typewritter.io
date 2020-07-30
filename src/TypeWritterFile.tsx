26d0a61d1cbec76bc9ec80849d2814a2ad86c408
Brian Czaicki <bczaicki@outlook.com>
2020-07-30 00:57:53 -0500

    Adding sass and making the styling of the typewritter

--- src/TypeWritterFile.tsx
+++ src/TypeWritterFile.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import "./App.css";
 import { TextField } from "@material-ui/core";
+import "./styling/main.css";
+import { StylesProvider } from "@material-ui/core/styles";
+import { css } from "@emotion/core";
 
 interface TWritterProps {
   typeWritterId: string;
@@ -14,16 +17,18 @@ function TypeWritterField({
   handleChange,
 }: TWritterProps) {
   return (
-    <TextField
-      multiline
-      onKeyDown={handleChange}
-      value={textValue}
-      id={typeWritterId}
-      autoFocus={true}
-      rows={16}
-      spellCheck={false}
-      InputProps={{ disableUnderline: true }}
-    />
+    <StylesProvider>
+      <TextField
+        multiline
+        onKeyDown={handleChange}
+        value={textValue}
+        id={typeWritterId}
+        autoFocus={true}
+        rows={16}
+        spellCheck={false}
+        InputProps={{ disableUnderline: true }}
+      />
+    </StylesProvider>
   );
 }
 
